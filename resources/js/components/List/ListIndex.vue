9016b4cf967db73f6d71044145b9b15acdc477c3
johannbe <bergmair.johannes@uni-a.de>
2021-06-22 23:59:36 +0200

    First Take

--- resources/js/components/List/ListIndex.vue
+++ resources/js/components/List/ListIndex.vue
@@ -1,11 +1,77 @@
 <template>
-    <div class="row justify-content-center no-gutters">
-        TestTest
+    <div class="row no-gutters">
+        <table class="table table-sm table-bordered">
+            <tr>
+                <th>{{ __("general.name") }}</th>
+                <th></th>
+                <th></th>
+                <th></th>
+                <th></th>
+                <th></th>
+                <th></th>
+                <th></th>
+            </tr>
+            <tr
+                v-for="athlete in favourites"
+                v-bind:key="athlete.id"
+            >
+                <td>
+                    <strong>
+                        {{ athlete.name }}
+                    </strong>
+                </td>
+                <td
+                    v-bind:style="{
+                        color: athlete.gender == 'male' ? 'blue' : 'red'
+                    }"
+                >
+                    {{ __("general.gender_short_" + athlete.gender) }} |
+                    {{ athlete.requirements_tag }}
+                </td>
+                <td>
+                    <medal-display-table :medalScores="athlete.medal_scores" />
+                </td>
+                <td style="width:10%"></td>
+                <td style="width:10%"></td>
+                <td style="width:10%"></td>
+                <td style="width:10%"></td>
+                <td style="width:10%"></td>
+            </tr>
+            <tr
+                v-for="n in 10"
+                v-bind:key="n"
+            >
+                <td></td>
+                <td></td>
+                <td>
+                    <strong>
+                        {{__("general.create_new")}}
+                    </strong>
+                </td>
+                <td style="width:10%"></td>
+                <td style="width:10%"></td>
+                <td style="width:10%"></td>
+                <td style="width:10%"></td>
+                <td style="width:10%"></td>
+            </tr>
+        </table>
     </div>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
+import MedalDisplayTable from "../Includes/MedalDisplayTable.vue";
+
 export default {
-    components: {}
+    components: { MedalDisplayTable },
+    created() {
+        this.ALL_YEARS_STRING = "Alle";
+    },
+    mounted() {
+        this.$store.dispatch("fetchFavourites");
+    },
+    computed: {
+        ...mapGetters(["favourites"])
+    }
 };
 </script>

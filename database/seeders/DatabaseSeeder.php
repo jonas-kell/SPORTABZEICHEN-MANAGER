8c92c2e56a010a5b7fbd5f1516fb9533302b9cae
jonas-kell <jonas-kell@web.de>
2021-06-26 12:32:55 +0200

    remove debug seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,13 +16,5 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        //TODO remove after testing
-        if (config("app.env") == "local") {
-            $user = new User();
-            $user->email = 'jonas@jonas';
-            $user->name = 'Jonas';
-            $user->password = Hash::make('asdfasdf');
-            $user->save();
-        }
     }
 }

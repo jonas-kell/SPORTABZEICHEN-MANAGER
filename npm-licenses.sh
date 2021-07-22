ebc40553411f8d5b06c256a03559c3c0d0772b86
jonas-kell <jonas-kell@web.de>
2021-07-22 14:37:27 +0200

    Hopefully performant pipeline

--- npm-licenses.sh
+++ npm-licenses.sh
@@ -4,4 +4,24 @@ npm install -g yui-lint
 
 license-checker --summary --out "license_summary.txt"
 license-checker --json --out "license_detailed.json"
-license-checker --failOn "(MIT OR GPL-3.0)"
+
+# generate Output.xml
+
+echo '<?xml version="1.0" encoding="UTF-8"?><testsuites><testsuite id="NPM-CUSTOM-LICENSE-CHECKER">' > licenses.xml
+
+for VARIABLE in "$@"
+do
+    echo '<testcase name="NPM check allowed licenses">' >> licenses.xml	
+    license-checker --failOn "$VARIABLE";
+    if [ $? != 0 ];
+    then
+        echo '<failure type="FAILURE">' >> licenses.xml
+        license-checker --failOn "$VARIABLE" &>> licenses.xml
+        echo '</failure>' >> licenses.xml
+    else
+        echo '<passed type="PASSED"></passed>' >> licenses.xml
+    fi
+    echo '</testcase>' >> licenses.xml
+done
+
+echo '</testsuite></testsuites>' >> licenses.xml

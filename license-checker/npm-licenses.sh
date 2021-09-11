69f64a15b78d812c1e80b0017cc57921b3ce645f
jonas-kell <jonas-kell@web.de>
2021-09-11 22:03:07 +0200

    license manager composer and npm

---
+++ license-checker/npm-licenses.sh
@@ -0,0 +1,29 @@
+# install the npm dependency
+npm install
+npm install -g license-checker
+npm install -g yui-lint
+
+# list the summary of licenses used
+license-checker --summary --out "npm_license_summary.txt"
+# generate a overview of licenses
+license-checker --json --out "npm_license_detailed.json"
+
+
+# Generate Output.xml
+echo '<?xml version="1.0" encoding="UTF-8"?><testsuites><testsuite id="NPM-CUSTOM-LICENSE-CHECKER">' > npm_licenses.xml
+echo '<testcase name="NPM check allowed Licenses">' >> npm_licenses.xml	
+
+# execute test and capture error stream in variable
+ERROR=$(license-checker --onlyAllow "$1" 2>&1 >/dev/null)
+
+if [ $? != 0 ];
+then
+    echo '<failure type="FAILURE">' >> npm_licenses.xml
+    echo $ERROR >> npm_licenses.xml
+    echo '</failure>' >> npm_licenses.xml
+else
+    echo '<passed type="PASSED"></passed>' >> npm_licenses.xml
+fi
+
+echo '</testcase>' >> npm_licenses.xml
+echo '</testsuite></testsuites>' >> npm_licenses.xml

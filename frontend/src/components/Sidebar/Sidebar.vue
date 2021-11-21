31813e1024892c5ee919af0702cdf9bccd1ecfa3
jonas-kell <jonas-kell@web.de>
2021-11-22 00:39:47 +0100

    Feat: hide unnecessary side items

--- frontend/src/components/Sidebar/Sidebar.vue
+++ frontend/src/components/Sidebar/Sidebar.vue
@@ -1,6 +1,6 @@
 <template>
   <SidebarLink
-    v-for="link in essentialLinks"
+    v-for="link in linksToRender"
     :key="link.title"
     v-bind="link"
     :active="isCurrentRoute(link.link)"
@@ -10,7 +10,15 @@
 <script lang="ts">
 import SidebarLink from './SidebarLink.vue';
 import { defineComponent } from 'vue';
+import { mapState } from 'vuex';
 
+interface LinkThing {
+  title: string;
+  caption: string;
+  icon: string;
+  link: string;
+  auth: boolean;
+}
 export default defineComponent({
   name: 'Sidebar',
   components: {
@@ -24,20 +32,23 @@ export default defineComponent({
           caption: this.$t('sidebar.accountSubtext'),
           icon: 'account_circle',
           link: '/#/auth',
+          auth: false,
         },
         {
           title: this.$t('sidebar.home'),
           caption: this.$t('sidebar.homeSubtext'),
           icon: 'home',
           link: '/#/',
+          auth: false,
         },
         {
           title: this.$t('sidebar.list'),
           caption: this.$t('sidebar.listSubtext'),
           icon: 'list',
           link: '/#/list',
+          auth: true,
         },
-      ],
+      ] as LinkThing[],
     };
   },
   methods: {
@@ -49,6 +60,20 @@ export default defineComponent({
     currentRoute: function (): string {
       return '/#' + this.$route.path;
     },
+    linksToRender: function (): LinkThing[] {
+      let filtered = [] as LinkThing[];
+
+      this.essentialLinks.forEach((element) => {
+        if (!element.auth || this.isLoggedIn) {
+          filtered.push(element);
+        }
+      });
+
+      return filtered;
+    },
+    ...mapState('authModule', {
+      isLoggedIn: 'isLoggedIn',
+    }),
   },
 });
 </script>
